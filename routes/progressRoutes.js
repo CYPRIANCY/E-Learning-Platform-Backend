7a69ab49fc8d48f2d9cf00db1d6170d103465eee
CYPRIANCY <akpensesughcyprian@gmail.com>
2025-06-08 22:12:57 +0100

    updated

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import {
-  markLessonComplete,
-  getProgressByCourse,
-} from '../controllers/progressController.js';
+// import express from 'express';
+// import {
+//   markLessonComplete,
+//   getProgressByCourse,
+// } from '../controllers/progressController.js';
 
-import { protect, authorizeRoles } from '../middlewares/authMiddleware.js';
+// import { protect, authorizeRoles } from '../middlewares/authMiddleware.js';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.use(protect, authorizeRoles('student'));
+// router.use(protect, authorizeRoles('student'));
 
-router.post('/complete', markLessonComplete);
-router.get('/:courseId', getProgressByCourse);
+// router.post('/complete', markLessonComplete);
+// router.get('/:courseId', getProgressByCourse);
 
-export default router;
+// export default router;
